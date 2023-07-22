489bfa752c55dbc95438e5e1d44ddb2781ee1e57
struk49 <andrewstruk@yahoo.co.uk>
2023-07-22 14:30:22 +0000

    added function to add department and push to database

--- staffmanager/routes.py
+++ staffmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from staffmanager import app, db
 from staffmanager.models import Department, Employee
 
@@ -6,3 +6,18 @@ from staffmanager.models import Department, Employee
 @app.route("/")
 def home():
     return render_template("employee.html")
+
+
+@app.route("/departments")
+def departments():
+    return render_template("departments.html")
+
+
+@app.route("/add_department", methods=["GET", "POST"])
+def add_department():
+    if request.method == "POST":
+        department = Department(department_name=request.form.get("department_name"))
+        db.session.add(department)
+        db.session.commit()
+        return redirect(url_for("departments"))
+    return render_template("add_department.html")
